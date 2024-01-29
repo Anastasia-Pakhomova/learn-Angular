0b9080314e9e128a5e968c2644ff90397b29e34c
pakhomova <pakhomova@kraud.ru>
2024-01-29 19:50:20 +0300

    task 7

--- src/app/services/courses/courses.service.ts
+++ src/app/services/courses/courses.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CourseInterface } from 'src/app/interfaces/course';
 import { coursesData } from './data';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
+   private baseUrl = "http://localhost:3000"
    private courses: CourseInterface[] = []
    private courseList: CourseInterface[] = []
 
-  constructor() { 
+  constructor(private httpClient: HttpClient) {
     this.courses = coursesData
   }
 
-  public getList(): CourseInterface[] {
-    return this.courses
+  public getList(): Observable<CourseInterface[]> {
+    return this.httpClient.get<CourseInterface[]>(`${this.baseUrl}/courses`)
   }
 
   public createCourse() {
